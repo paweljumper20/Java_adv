8157909fd095c3749e10944a60df4900348b9d91
paweljumper20 <paweljumper20@gmail.com>
2019-12-12 19:43:16 +0100

    12.12.2019

--- src/main/java/fileio/FileService.java
+++ src/main/java/fileio/FileService.java
@@ -1,7 +1,6 @@
 package fileio;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.Scanner;
 
 public class FileService {
@@ -18,6 +17,8 @@ public class FileService {
         FileService fs = new FileService();
         fs.readFile("C:\\Users\\LEGION\\Desktop\\projects\\java_adv\\src\\main\\java\\fileio\\text.txt");
         fs.readCsvFile("C:\\Users\\LEGION\\Desktop\\projects\\java_adv\\src\\main\\java\\fileio\\data.csv");
+        fs.insertDataIntoFile("C:\\Users\\LEGION\\Desktop\\projects\\java_adv\\src\\main\\java\\fileio\\fromFileer.txt",
+                "Ala ma kota");
     }
 
     public void readCsvFile(String path) throws FileNotFoundException {
@@ -32,12 +33,29 @@ public class FileService {
             if (i == 1) {
                 continue;
             }
+
             String elements[] = line.split(";");
             System.out.println("ID: " + elements[0]);
             System.out.println("NAME: " + elements[1]);
             System.out.println("LASTNAME: " + elements[2]);
             System.out.println("==========================");
+
         }
+        s.close();
     }
+
+    public void insertDataIntoFile(String path,String data) throws FileNotFoundException {
+        PrintWriter pw = new PrintWriter(new File(path));
+        pw.println("NAGŁÓWEK");
+        pw.println(data);
+        pw.close();
+    }
+
+    public void appendDataIntoFile(String path,String data) throws IOException {
+        FileWriter fw = new FileWriter(new File(path),true);
+        fw.append(data);
+        fw.close();
+    }
+
 
 }
